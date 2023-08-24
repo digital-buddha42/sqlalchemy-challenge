869ba44efccd4bc1926b19d6bb9869f45ec2193e
Alec Banerji <alec.banerji@gmail.com>
2023-08-23 23:47:01 -0700

    Successfully added first route!

--- Chillin/app.py
+++ Chillin/app.py
@@ -1,5 +1,7 @@
 # Import the dependencies.
 import numpy as np
+import pandas as pd
+import datetime as dt
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -38,8 +40,6 @@ session = Session(engine)
 #################################################
 app = Flask(__name__)
 
-
-
 #################################################
 # Flask Routes
 #################################################
@@ -52,11 +52,57 @@ def welcome():
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/<start>/<end><br/>"
 
     )
 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
+    # Convert the query results from your precipitation analysis
+    #  (i.e. retrieve only the last 12 months of data) to a dictionary 
+    # using date as the key and prcp as the value.
+
+    # Return the JSON representation of your dictionary.
+
+    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+
+    # Convert to datetime object
+    most_recent_date = pd.to_datetime(most_recent_date[0])
+
+    # See the date 12 months ago
+    twelve_months = most_recent_date - dt.timedelta(days=365)
+
+    # Convert to correct format
+    twelve_months = twelve_months.strftime("%Y-%m-%d")
+
+    # Perform a query to retrieve the date and precipitation scores
+    twelve_months_precipitation = session.query(Measurement.date,Measurement.prcp).filter(Measurement.date >= twelve_months).all()
+
+    # Save the query results as a Pandas DataFrame. Explicitly set the column names
+    precipitation_df = pd.DataFrame(twelve_months_precipitation, columns=['Date', 'Precipitation'])
+
+    # Sort the dataframe by date
+    precipitation_df = precipitation_df.sort_values(by='Date')
+    
+    session.close()
+    
+    prcp_list = []
+    # for date, prcp in precipitation_df:
+    #     precipitation_dict = {}
+    #     precipitation_dict["Date"] = date
+    #     precipitation_dict["Precipitation"] = prcp
+    #     prcp_list.append(precipitation_dict)
+
+    for index, row in precipitation_df.iterrows():
+        precipitation_dict = {}
+        precipitation_dict["Date"] = row["Date"]
+        precipitation_dict["Precipitation"] = row["Precipitation"]
+        prcp_list.append(precipitation_dict)
+
+    return jsonify(prcp_list)
 
 
 
